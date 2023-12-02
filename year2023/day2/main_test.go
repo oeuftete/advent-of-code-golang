57095d2645209604c2e0c1f6d024e82b698ff142
Ken Crowell <oeuftete@gmail.com>
2023-12-02 15:30:25 -0400

    Add year 2023, day 2, part a

---
+++ year2023/day2/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	ExampleOne string = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+	ExampleOneAnswerA int = 8
+)
+
+func TestAnswers(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		answerA int
+	}{
+		{
+			name:    "example one answer",
+			input:   ExampleOne,
+			answerA: ExampleOneAnswerA,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			answerA, _ := answers(test.input)
+			assert.Equal(t, test.answerA, answerA, "Answer A incorrect")
+		})
+	}
+}
